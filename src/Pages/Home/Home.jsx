2f156fc0a8bcb8536c1b19f919d9c2456f6b113a
Tansimrinky <tansimrinkybindu@gmail.com>
2023-11-25 23:28:27 +0600

    banner section added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import { Helmet } from "react-helmet-async";
+import Banner from "../../Components/Banner/Banner";
 
 const Home = () => {
     return (
@@ -6,7 +7,7 @@ const Home = () => {
             <Helmet>
                 <title>SkillBoost Hub || Home</title>
             </Helmet>
-            This is Home
+            <Banner></Banner>
         </div>
     );
 };
