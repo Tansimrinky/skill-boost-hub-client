743c1fcf282326c6b11c89fe2048a92b57d6f26c
Tansimrinky <tansimrinkybindu@gmail.com>
2023-11-30 23:14:46 +0600

    Reviesion done

--- src/Components/Information/Information.jsx
+++ src/Components/Information/Information.jsx
@@ -1,26 +1,19 @@
 import { useEffect, useState } from "react";
-import useAxiosSecure, { axiosSecure } from "../../hooks/useAxiosSecure/useAxiosSecure";
+import useAxiosSecure from "../../hooks/useAxiosSecure/useAxiosSecure";
 import useEnrollClasses from "../../hooks/useEnrollClasses/useEnrollClasses";
 import useCourses from "../../hooks/useCourses/useCourses";
-import { FaUsers } from "react-icons/fa";
+import useUser from "../../hooks/useUser/useUser";
+
 
 
 const Information = () => {
-    const axiosSecure = useAxiosSecure()
     const [ enrollClass ] = useEnrollClasses()
-    const [user, setUser] = useState([]);
     const [ courses] = useCourses()
-     useEffect( () => {
-        axiosSecure.get('/users')
-        .then(data => {
-            setUser(data.data)
-        })
-       
-    }, [axiosSecure])
+    const [users] = useUser()
     return (
         <div className="flex gap-2">
             <div className="flex justify-between border-black  gap-6 text-white font-bold">
-                 <div className="bg-blue-500  p-10 ">Total User: {user.length} <br />
+                 <div className="bg-blue-500  p-10 ">Total User: {users.length} <br />
                  </div>
                  <div className="bg-blue-500 p-10 ">Total Courses:{courses.length}</div>
                  <div  className="bg-blue-500  p-10 "> Total Enrolled Classes:{enrollClass.length}</div>
--- src/hooks/useUser/useUser.jsx
+++ src/hooks/useUser/useUser.jsx
@@ -1,2 +1,22 @@
+import { useEffect, useState } from "react";
 
+
+
+const useUser = () => {
+
+    const [users, setUsers ] = useState([]);
+    const [ loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://skill-boost-hub-server.vercel.app/users')
+        .then(res => res.json())
+        .then(data => {
+            setUsers(data);
+            setLoading(false)
+        })
+    }, [])
+    return [users, loading]
+};
+
+export default useUser;
 
